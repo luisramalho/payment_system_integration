228243347cb2705384674f87ec5b4d2d3390ac63
Luís Ramalho <info@luisramalho.com>
2014-01-27 11:07:41 +0000

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Evvnt::Application.config.secret_key_base = '22d3f655f0afbc07b3389664fcba707f3b51335180aa7165d308d242a33e4ee9e773207f5e20c7ff0c5be60a6afec94c0aa3584fca5512e9272b94d9c1039aa9'
